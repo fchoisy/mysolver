a5dfaa28bde9c50a0175770d371c5a9bf5a83004
Floris Choisy <floris.choisy@gmail.com>
2021-05-10 18:53:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+We need:
+- GLEW
+- GLFW
+- GLM
+- tdogl
+
+On macOS the easiest way seems to install via homebrew.
+
+How to include external libraries in C++.
+Multiple strategies.
+- Building source code.
+- Link against precompiled binaries.
+
+- dependencies/
+    - include/
+    - lib/
+
+TODO:
+- include glfw
+- compile and include glew
+- compile and include glm
+
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// third-party libraries
+#include <GL/glew.h>  // Runtime loading of OpenGL API functions
+#include <GLFW/glfw3.h>  // Windowing
+#include <glm/glm.hpp>  // Vector maths
+
+int main() {
+    // initialise GLFW
+    // glfwSetErrorCallback(OnError);
+    if(!glfwInit())
+        throw std::runtime_error("glfwInit failed");
+    
+    // open a window with GLFW
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* gWindow = glfwCreateWindow((int)500, (int)500, "OpenGL Tutorial", NULL, NULL);
+    if(!gWindow)
+        throw std::runtime_error("glfwCreateWindow failed. Can your hardware handle OpenGL 3.2?");
+
+    // GLFW settings
+    glfwMakeContextCurrent(gWindow);
+    
+    // initialise GLEW
+    glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
+    if(glewInit() != GLEW_OK)
+        throw std::runtime_error("glewInit failed");
+
+    // print out some info about the graphics drivers
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    // GLFWwindow* window = glfwCreateWindow(600, 400, "Super title", NULL, NULL);
+
+    std::cout << "hello world\n";
+
+    while (!glfwWindowShouldClose(gWindow)) {
+        glfwPollEvents();
+    }
+}
