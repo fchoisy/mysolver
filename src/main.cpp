4a09ecc799cbdeb9625067c3dea49a6ebf2efc29
Floris Choisy <floris.choisy@gmail.com>
2021-05-11 19:01:54 +0200

    Compiles and runs but black

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,180 @@
-#include <iostream>
+/**
+ * main
+ * 
+ * The structure of this file is inspired by Tom Dalling's OpenGL tutorial (http://tomdalling.com/).
+ */
+
+
 // third-party libraries
 #include <GL/glew.h>  // Runtime loading of OpenGL API functions
 #include <GLFW/glfw3.h>  // Windowing
-#include <glm/glm.hpp>  // Vector maths
-
+#include <glm/gtc/matrix_transform.hpp> // Vector maths
 #include <tdogl/Program.h>
+#include <tdogl/Shader.h>
+#include <tdogl/Texture.h>
+#include <tdogl/Bitmap.h>
+// standard C++ libraries
+#include <iostream>
 
-int main() {
+
+// constants
+const glm::vec2 SCREEN_SIZE(800, 600);
+
+// globals
+GLFWwindow* gWindow = NULL;
+tdogl::Texture* gTexture = NULL;
+tdogl::Program* gProgram = NULL;
+GLuint gVAO = 0;
+GLuint gVBO = 0;
+GLfloat gDegreesRotated = 0.0f;
+
+static void LoadTexture() {
+    tdogl::Bitmap bmp = tdogl::Bitmap::bitmapFromFile("./resources/circle.png");
+    bmp.flipVertically();
+    gTexture = new tdogl::Texture(bmp);
+}
+
+static void LoadShaders() {
+    std::vector<tdogl::Shader> shaders;
+    shaders.push_back(tdogl::Shader::shaderFromFile("./resources/vertex-shader.txt", GL_VERTEX_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile("./resources/fragment-shader.txt", GL_FRAGMENT_SHADER));
+    gProgram = new tdogl::Program(shaders);
+    
+    gProgram->use();
+//    glm::mat4 projection = glm::ortho(-SCREEN_SIZE.x/2, -SCREEN_SIZE.x/2, SCREEN_SIZE.y/2, SCREEN_SIZE.y/2);
+//    glm::mat4 projection = glm::ortho(-1.f, 1.f, -1.f, 1.f);
+    glm::mat4 projection = glm::scale(glm::mat4(), glm::vec3(SCREEN_SIZE.y/SCREEN_SIZE.x, 1.f, 1.f));
+    
+    
+//    perspective(glm::radians(50.0f), SCREEN_SIZE.x/SCREEN_SIZE.y, 0.1f, 10.0f);
+    gProgram->setUniform("projection", projection);
+//    glm::mat4 camera = glm::lookAt(glm::vec3(0,0,0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
+//    gProgram->setUniform("camera", camera);
+    gProgram->stopUsing();
+}
+
+static void LoadCube() {
+    glGenVertexArrays(1, &gVAO);
+    glBindVertexArray(gVAO);
+    
+    glGenBuffers(1, &gVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+    
+    GLfloat vertexData[] = {
+        //  X     Y     Z       U     V
+        // bottom
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+
+        // top
+        -1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+         1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+         1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+         1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+        // front
+        -1.0f,-1.0f, 1.0f,   1.0f, 0.0f,
+         1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+         1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+         1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+        // back
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+         1.0f, 1.0f,-1.0f,   1.0f, 1.0f,
+
+        // left
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+
+        // right
+         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+         1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+         1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+         1.0f, 1.0f, 1.0f,   0.0f, 1.0f
+    };
+    
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+    
+    GLsizei stride = 5 * sizeof(GLfloat);
+    
+    glEnableVertexAttribArray(gProgram->attrib("vert"));
+    glVertexAttribPointer(gProgram->attrib("vert"), 3, GL_FLOAT, GL_FALSE, stride, NULL);
+    
+    glEnableVertexAttribArray(gProgram->attrib("vertTexCoord"));
+    glVertexAttribPointer(gProgram->attrib("vertTexCoord"), 2, GL_FLOAT, GL_TRUE, stride, (const GLvoid*)(3 * sizeof(GLfloat)));
+    
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+static void Update(float secondsElapsed) {
+//    static const GLfloat degreesPerSecond = 90.0f;
+//    gDegreesRotated += secondsElapsed * degreesPerSecond;
+//    
+//    while (gDegreesRotated > 360.0f) gDegreesRotated -= 360.0f;
+}
+
+static void Render() {
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    gProgram->use();
+    
+//    gProgram->setUniform("model", glm::rotate(glm::mat4(), glm::radians(gDegreesRotated), glm::vec3(0, 1, 0)));
+    
+
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, gTexture->object());
+    gProgram->setUniform("tex", 0);
+    
+    glBindVertexArray(gVAO);
+    
+    gProgram->setUniform("color", glm::vec4(0, 0, 1, 1));
+    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(2, 2, 0)));
+    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
+    
+    gProgram->setUniform("color", glm::vec4(1, 0, 0, 1));
+    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(0, 2, 0)));
+    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
+    
+    gProgram->setUniform("color", glm::vec4(0, 1, 0, 1));
+    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(0, 0, 0)));
+    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
+    
+    glBindVertexArray(0);
+    
+    gProgram->stopUsing();
+    glfwSwapBuffers(gWindow);
+}
+
+void OnError(int errorCode, const char* msg) {
+    throw std::runtime_error(msg);
+}
+
+// the program starts here
+void AppMain() {
     // initialise GLFW
-    // glfwSetErrorCallback(OnError);
+    glfwSetErrorCallback(OnError);
     if(!glfwInit())
         throw std::runtime_error("glfwInit failed");
     
@@ -18,28 +184,77 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow* gWindow = glfwCreateWindow((int)500, (int)500, "OpenGL Tutorial", NULL, NULL);
+    gWindow = glfwCreateWindow((int)SCREEN_SIZE.x, (int)SCREEN_SIZE.y, "OpenGL Tutorial", NULL, NULL);
     if(!gWindow)
         throw std::runtime_error("glfwCreateWindow failed. Can your hardware handle OpenGL 3.2?");
 
     // GLFW settings
     glfwMakeContextCurrent(gWindow);
-    
+
     // initialise GLEW
     glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
     if(glewInit() != GLEW_OK)
         throw std::runtime_error("glewInit failed");
+    
+    // GLEW throws some errors, so discard all the errors so far
+    while(glGetError() != GL_NO_ERROR) {}
 
     // print out some info about the graphics drivers
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
     std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    // GLFWwindow* window = glfwCreateWindow(600, 400, "Super title", NULL, NULL);
 
-    std::cout << "hello world\n";
+    // make sure OpenGL version 3.2 API is available
+    if(!GLEW_VERSION_3_2)
+        throw std::runtime_error("OpenGL 3.2 API is not available.");
 
-    while (!glfwWindowShouldClose(gWindow)) {
+    // OpenGL settings
+    glEnable(GL_DEPTH_TEST);  // Enable depth buffering
+    glDepthFunc(GL_LESS);  // The pixels with less depth will be drawn on top
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // load vertex and fragment shaders into opengl
+    LoadShaders();
+
+    // load the texture
+    LoadTexture();
+
+    // create buffer and fill it with the points of the cube
+    LoadCube();
+
+    // run while the window is open
+    double lastTime = glfwGetTime();
+    while(!glfwWindowShouldClose(gWindow)){
+        // process pending events
         glfwPollEvents();
+        
+        // update the scene based on the time elapsed since last update
+        double thisTime = glfwGetTime();
+        Update((float)(thisTime - lastTime));
+        lastTime = thisTime;
+        // draw one frame
+        Render();
+
+        // check for errors
+        GLenum error = glGetError();
+        if(error != GL_NO_ERROR)
+            std::cerr << "OpenGL Error " << error << std::endl;
     }
+
+    // clean up and exit
+    glfwTerminate();
+}
+
+
+int main(int argc, char *argv[]) {
+    try {
+        AppMain();
+    } catch (const std::exception& e){
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
