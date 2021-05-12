0b03e973d64a521b12eac27d6703af8ef69302bc
Floris Choisy <floris.choisy@gmail.com>
2021-05-12 19:28:27 +0200

    Works from any folder thanks to RootDir.h.in

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,43 @@
  * The structure of this file is inspired by Tom Dalling's OpenGL tutorial (http://tomdalling.com/).
  */
 
-
 // third-party libraries
 #include <GL/glew.h>  // Runtime loading of OpenGL API functions
 #include <GLFW/glfw3.h>  // Windowing
 #include <glm/gtc/matrix_transform.hpp> // Vector maths
 #include <tdogl/Program.h>
-#include <tdogl/Shader.h>
 #include <tdogl/Texture.h>
-#include <tdogl/Bitmap.h>
 // standard C++ libraries
 #include <iostream>
 
+#include "helpers/RootDir.h"
 
 // constants
 const glm::vec2 SCREEN_SIZE(800, 600);
 
 // globals
 GLFWwindow* gWindow = NULL;
-tdogl::Texture* gTexture = NULL;
+// tdogl::Texture* gTexture = NULL;
 tdogl::Program* gProgram = NULL;
 GLuint gVAO = 0;
 GLuint gVBO = 0;
-GLfloat gDegreesRotated = 0.0f;
 
-static void LoadTexture() {
-    tdogl::Bitmap bmp = tdogl::Bitmap::bitmapFromFile("./resources/circle.png");
-    bmp.flipVertically();
-    gTexture = new tdogl::Texture(bmp);
-}
+// static void LoadTexture() {
+//     tdogl::Bitmap bmp = tdogl::Bitmap::bitmapFromFile("./resources/circle.png");
+//     bmp.flipVertically();
+//     gTexture = new tdogl::Texture(bmp);
+// }
 
 static void LoadShaders() {
     std::vector<tdogl::Shader> shaders;
-    shaders.push_back(tdogl::Shader::shaderFromFile("./resources/vertex-shader.txt", GL_VERTEX_SHADER));
-    shaders.push_back(tdogl::Shader::shaderFromFile("./resources/fragment-shader.txt", GL_FRAGMENT_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile(ROOT_DIR"resources/vertex-shader.txt", GL_VERTEX_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile(ROOT_DIR"/resources/fragment-shader.txt", GL_FRAGMENT_SHADER));
     gProgram = new tdogl::Program(shaders);
     
-    gProgram->use();
-//    glm::mat4 projection = glm::ortho(-SCREEN_SIZE.x/2, -SCREEN_SIZE.x/2, SCREEN_SIZE.y/2, SCREEN_SIZE.y/2);
-//    glm::mat4 projection = glm::ortho(-1.f, 1.f, -1.f, 1.f);
-    glm::mat4 projection = glm::scale(glm::mat4(), glm::vec3(SCREEN_SIZE.y/SCREEN_SIZE.x, 1.f, 1.f));
-    
-    
-//    perspective(glm::radians(50.0f), SCREEN_SIZE.x/SCREEN_SIZE.y, 0.1f, 10.0f);
-    gProgram->setUniform("projection", projection);
-//    glm::mat4 camera = glm::lookAt(glm::vec3(0,0,0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
-//    gProgram->setUniform("camera", camera);
-    gProgram->stopUsing();
+    // gProgram->use();
+    // glm::mat4 projection = glm::perspective(glm::radians(50.0f), SCREEN_SIZE.x/SCREEN_SIZE.y, 0.1f, 10.0f);
+    // gProgram->setUniform("projection", projection);
+    // gProgram->stopUsing();
 }
 
 static void LoadCube() {
@@ -60,55 +50,61 @@ static void LoadCube() {
     glGenBuffers(1, &gVBO);
     glBindBuffer(GL_ARRAY_BUFFER, gVBO);
     
+    // GLfloat vertexData[] = {
+    //     //  X     Y     Z       U     V
+    //     // bottom
+    //     -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+    //      1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+    //     -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+    //      1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+    //      1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+    //     -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+
+    //     // top
+    //     -1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+    //     -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+    //      1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+    //      1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+    //     -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+    //      1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+    //     // front
+    //     -1.0f,-1.0f, 1.0f,   1.0f, 0.0f,
+    //      1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+    //     -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+    //      1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+    //      1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+    //     -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+
+    //     // back
+    //     -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+    //     -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+    //      1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+    //      1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+    //     -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+    //      1.0f, 1.0f,-1.0f,   1.0f, 1.0f,
+
+    //     // left
+    //     -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+    //     -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+    //     -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+    //     -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+    //     -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+    //     -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+
+    //     // right
+    //      1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+    //      1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+    //      1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+    //      1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+    //      1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+    //      1.0f, 1.0f, 1.0f,   0.0f, 1.0f
+    // };
+
     GLfloat vertexData[] = {
-        //  X     Y     Z       U     V
-        // bottom
-        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
-         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
-        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
-         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
-         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
-
-        // top
-        -1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
-        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-         1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
-         1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
-        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-         1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-
-        // front
-        -1.0f,-1.0f, 1.0f,   1.0f, 0.0f,
-         1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
-        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-         1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
-         1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-
-        // back
-        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
-        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
-         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
-         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
-        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
-         1.0f, 1.0f,-1.0f,   1.0f, 1.0f,
-
-        // left
-        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
-        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
-        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
-
-        // right
-         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
-         1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
-         1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
-         1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
-         1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
-         1.0f, 1.0f, 1.0f,   0.0f, 1.0f
+        -1.0f,-1.0f, 0.5f,   0.0f, 1.0f,
+         0.0f, 1.0f, 0.5f,   1.0f, 0.0f,
+         1.0f,-1.0f, 0.5f,   0.0f, 0.0f,
     };
     
     
@@ -119,8 +115,8 @@ static void LoadCube() {
     glEnableVertexAttribArray(gProgram->attrib("vert"));
     glVertexAttribPointer(gProgram->attrib("vert"), 3, GL_FLOAT, GL_FALSE, stride, NULL);
     
-    glEnableVertexAttribArray(gProgram->attrib("vertTexCoord"));
-    glVertexAttribPointer(gProgram->attrib("vertTexCoord"), 2, GL_FLOAT, GL_TRUE, stride, (const GLvoid*)(3 * sizeof(GLfloat)));
+    // glEnableVertexAttribArray(gProgram->attrib("vertTexCoord"));
+    // glVertexAttribPointer(gProgram->attrib("vertTexCoord"), 2, GL_FLOAT, GL_TRUE, stride, (const GLvoid*)(3 * sizeof(GLfloat)));
     
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -128,10 +124,7 @@ static void LoadCube() {
 }
 
 static void Update(float secondsElapsed) {
-//    static const GLfloat degreesPerSecond = 90.0f;
-//    gDegreesRotated += secondsElapsed * degreesPerSecond;
-//    
-//    while (gDegreesRotated > 360.0f) gDegreesRotated -= 360.0f;
+    ;
 }
 
 static void Render() {
@@ -139,26 +132,15 @@ static void Render() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     gProgram->use();
     
-//    gProgram->setUniform("model", glm::rotate(glm::mat4(), glm::radians(gDegreesRotated), glm::vec3(0, 1, 0)));
-    
-
-    
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, gTexture->object());
-    gProgram->setUniform("tex", 0);
+    // glActiveTexture(GL_TEXTURE0);
+    // glBindTexture(GL_TEXTURE_2D, gTexture->object());
+    // gProgram->setUniform("tex", 0);
     
     glBindVertexArray(gVAO);
     
     gProgram->setUniform("color", glm::vec4(0, 0, 1, 1));
-    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(2, 2, 0)));
-    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
-    
-    gProgram->setUniform("color", glm::vec4(1, 0, 0, 1));
-    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(0, 2, 0)));
-    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
-    
-    gProgram->setUniform("color", glm::vec4(0, 1, 0, 1));
-    gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(0, 0, 0)));
+    // gProgram->setUniform("model", glm::mat4());
+    // gProgram->setUniform("model", glm::translate(glm::scale(glm::mat4(), glm::vec3(.1f, .1f, .1f)), glm::vec3(2, 2, 0)));
     glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
     
     glBindVertexArray(0);
@@ -210,8 +192,8 @@ void AppMain() {
         throw std::runtime_error("OpenGL 3.2 API is not available.");
 
     // OpenGL settings
-    glEnable(GL_DEPTH_TEST);  // Enable depth buffering
-    glDepthFunc(GL_LESS);  // The pixels with less depth will be drawn on top
+    // glEnable(GL_DEPTH_TEST);  // Enable depth buffering
+    // glDepthFunc(GL_LESS);  // The pixels with less depth will be drawn on top
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -219,7 +201,7 @@ void AppMain() {
     LoadShaders();
 
     // load the texture
-    LoadTexture();
+    // LoadTexture();
 
     // create buffer and fill it with the points of the cube
     LoadCube();
@@ -236,11 +218,16 @@ void AppMain() {
         lastTime = thisTime;
         // draw one frame
         Render();
+        // std::cout << "Still alive at " << thisTime << std::endl;
 
         // check for errors
         GLenum error = glGetError();
         if(error != GL_NO_ERROR)
             std::cerr << "OpenGL Error " << error << std::endl;
+
+        if (glfwGetKey(gWindow, GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(gWindow, GL_TRUE);
+        }
     }
 
     // clean up and exit
