810d8f0f8cb7d6253592872ec4487cb93cd595e9
Floris Choisy <floris.choisy@gmail.com>
2021-06-23 11:06:39 +0200

    Correct viscosity acceleration

--- src/ParticleSimulation.cpp
+++ src/ParticleSimulation.cpp
@@ -38,42 +38,55 @@ void ParticleSimulation::UpdateParticles(const float timeStep, const glm::vec2 g
 {
     for (auto &&particleSet : particleSets)
     {
-        for (auto &&particle : particleSet->particles)
+        if (!particleSet->isStatic)
         {
-            particle.density = 0.f;
-            for (auto &&neighbor : GetNeighbors(particle))
+            // Compute density and pressure for each particle
+            for (auto &&particle : particleSet->particles)
             {
-                particle.density += neighbor->mass * KernelFunction(particle.position, neighbor->position, particleSet->spacing);
+                particle.density = 0.f;
+                for (auto &&neighbor : GetNeighbors(particle))
+                {
+                    particle.density += neighbor->mass * KernelFunction(particle.position, neighbor->position, particleSet->spacing);
+                }
+                particle.pressure = glm::max(particleSet->stiffness * (particle.density / particleSet->restDensity - 1.f), 0.f);
             }
-            particle.pressure = glm::max(particleSet->stiffness * (particle.density / particleSet->restDensity - 1.f), 0.f);
-        }
-
-        for (auto &&particle : particleSet->particles)
-        {
-            glm::vec2 viscosityAcceleration(0.f, 0.f);
-            for (auto &&neighbor : GetNeighbors(particle))
+            // Compute accelerations for each particle
+            for (auto &&particle : particleSet->particles)
             {
-                glm::vec2 positionDiff = neighbor->position - particle.position;
-                glm::vec2 velocityDiff = neighbor->velocity - particle.velocity;
-                viscosityAcceleration += (neighbor->mass / neighbor->density) * (velocityDiff * positionDiff) / (glm::dot(positionDiff, positionDiff) + 0.01f * particleSet->spacing * particleSet->spacing) * KernelDerivative(particle.position, neighbor->position, particleSet->spacing);
+                // Viscosity acceleration
+                glm::vec2 viscosityAcceleration(0.f, 0.f);
+                for (auto &&neighbor : GetNeighbors(particle))
+                {
+                    glm::vec2 positionDiff = neighbor->position - particle.position;
+                    glm::vec2 velocityDiff = neighbor->velocity - particle.velocity;
+                    glm::vec2 kernelDer = KernelDerivative(particle.position, neighbor->position, particleSet->spacing);
+                    viscosityAcceleration +=
+                        kernelDer *
+                        (neighbor->mass / neighbor->density) *
+                        (glm::dot(velocityDiff, positionDiff)) /
+                        (glm::dot(positionDiff, positionDiff) + 0.01f * particleSet->spacing * particleSet->spacing);
+                }
+                viscosityAcceleration *= 2.f; // dimensionality (2D)
+                viscosityAcceleration *= particleSet->viscosity;
+                // Pressure acceleration
+                glm::vec2 pressureAcceleration(0.f, 0.f);
+                for (auto &&neighbor : GetNeighbors(particle))
+                {
+                    pressureAcceleration += neighbor->mass * (particle.pressure / (particle.density * particle.density) + neighbor->pressure / (neighbor->density * neighbor->density)) * KernelDerivative(particle.position, neighbor->position, particleSet->spacing);
+                }
+                pressureAcceleration *= -1.f;
+                // Other accelerations
+                glm::vec2 otherAccelerations = gravity;
+                // Total acceleration
+                particle.acceleration = viscosityAcceleration + pressureAcceleration + otherAccelerations;
             }
-            viscosityAcceleration *= 2.f; // dimensionality (2D)
-            viscosityAcceleration *= particleSet->viscosity;
-
-            glm::vec2 pressureAcceleration(0.f, 0.f);
-            for (auto &&neighbor : GetNeighbors(particle))
+            // Update position based on acceleration for each particle
+            // using the semi-implicit Euler method
+            for (auto &&particle : particleSet->particles)
             {
-                pressureAcceleration += neighbor->mass * (particle.pressure / (particle.density * particle.density) + neighbor->pressure / (neighbor->density * neighbor->density)) * KernelDerivative(particle.position, neighbor->position, particleSet->spacing);
+                particle.velocity += timeStep * particle.acceleration;
+                particle.position += timeStep * particle.velocity;
             }
-            pressureAcceleration *= -1.f;
-
-            particle.acceleration = viscosityAcceleration + gravity + pressureAcceleration;
-        }
-
-        for (auto &&particle : particleSet->particles)
-        {
-            particle.velocity += timeStep * particle.acceleration;
-            particle.position += timeStep * particle.velocity;
         }
     }
 }
