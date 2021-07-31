95783dbcf0394ea440e909e13413172996180562
Floris Choisy <floris.choisy@gmail.com>
2021-07-31 10:20:18 +0200

    Put individual properties in history

--- src/HistoryTracker.hpp
+++ src/HistoryTracker.hpp
@@ -8,16 +8,17 @@ class HistoryTracker
 private:
     std::vector<float> timeHistory;
     std::vector<std::vector<Particle>> targetHistory;
-    // std::vector<float> densityHistory;
-    // std::vector<float> pressureHistory;
-    // std::vector<float> pressureAccelerationHistory;
-    // std::vector<float> viscosityAccelerationHistory;
-    // std::vector<float> otherAccelerationsHistory;
-    // std::vector<float> velocityHistory;
-    // std::map<const Particle *, std::map<std::string, std::vector<float>>> propertiesHistory;
     const ParticleSet *target;
 
 public:
+    // Properties of particle at index 0 over time
+    std::vector<float> density;
+    std::vector<float> pressure;
+    std::vector<float> pressureAcceleration;
+    std::vector<float> viscosityAcceleration;
+    std::vector<float> otherAccelerations;
+    std::vector<float> velocity;
+
     HistoryTracker()
         : target(nullptr)
     {
@@ -26,29 +27,24 @@ public:
     void SetTarget(const ParticleSet *target)
     {
         this->target = target;
-        // for (auto &&particle : target->particles)
-        // {
-        //     propertiesHistory[&particle] = std::map<std::string, std::vector<float>>();
-        // }
     }
 
     void Step(float currentTime)
     {
         if (target != nullptr)
         {
-            timeHistory.push_back(currentTime);
-            targetHistory.push_back(target->particles);
-
-            // for (auto &&particle : target->particles)
-            // {
-            //     propertiesHistory[&particle]["density"].push_back(3.f);
-            // }
-            // densityHistory.push_back(ps.at(0).density / 1000.f);
-            // pressureHistory.push_back(ps.at(0).pressure / 10000.f);
-            // pressureAccelerationHistory.push_back(glm::length(ps.at(0).pressureAcceleration) / 10.f);
-            // viscosityAccelerationHistory.push_back(glm::length(ps.at(0).viscosityAcceleration) / 10.f);
-            // otherAccelerationsHistory.push_back(glm::length(ps.at(0).otherAccelerations) / 10.f);
-            // velocityHistory.push_back(glm::length(ps.at(0).velocity));
+            if (target->particles.size() >= 1)
+            {
+                timeHistory.push_back(currentTime);
+                targetHistory.push_back(target->particles);
+                auto frontParticle = target->particles.front();
+                density.push_back(frontParticle.density / 1000.f);
+                pressure.push_back(frontParticle.pressure / 10000.f);
+                pressureAcceleration.push_back(glm::length(frontParticle.pressureAcceleration) / 10.f);
+                viscosityAcceleration.push_back(glm::length(frontParticle.viscosityAcceleration) / 10.f);
+                otherAccelerations.push_back(glm::length(frontParticle.otherAccelerations) / 10.f);
+                velocity.push_back(glm::length(frontParticle.velocity));
+            }
         }
     }
 
@@ -72,5 +68,11 @@ public:
         timeHistory.clear();
         targetHistory.clear();
         // propertiesHistory.clear();
+        density.clear();
+        pressure.clear();
+        pressureAcceleration.clear();
+        viscosityAcceleration.clear();
+        otherAccelerations.clear();
+        velocity.clear();
     }
 };
